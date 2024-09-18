f8516f9a2443b3c5b86a8739441654633e5fc924
Luke Maloney <lukemaloney@Lukes-MacBook-Pro-2.local>
2024-09-18 11:40:09 -0700

    initial commit

--- README.md
+++
@@ -1,3 +0,0 @@
-[![C++ CI](https://github.com/ChicoState/FirstIO/actions/workflows/actions.yml/badge.svg)](https://github.com/ChicoState/FirstIO/actions/workflows/actions.yml)
-
-# First IO project
--- main.cpp
+++ main.cpp
@@ -2,47 +2,6 @@
 using namespace std;
 
 int main(){
-  string input = ""
-  int num1 = 0;
-  int num2 = 0;
-
-  cout << "? ";
-  cin >> input;
-
-  if( input == "help" ){
-    cout << "Hi!\n";
-    cout << "Options:\nadd\nsubtract\nmultiply\ndivide\nhelp\nquit\n";
-  }
-  else if( input == "add" || input == "subtract" || input == "multiply" || input == "divide"){
-    cout << "First number: ";
-    cin >> num1;
-    cin >> num2;
-
-    if( input == "add" ){
-      cout << "=" << num1+num2 << endl;
-    }
-    else if( input == "subtract" ){
-      cout << "=" << num1-num2 << endl;
-    }
-    else if( input == "multiply" ){
-      cout << "=" << num1*num2 << endl;
-    }
-    else if( input == "divide" ){
-      if( num2 == 0 ){
-        cout << "Error: Cannot Divide by zero!\n";
-      }
-      else{
-        cout << "=" << num1/num2 << endl;
-      }
-    }
-      
-  }
-  else if( "quit" ){
-    cout << "Goodbye!\n";
-  }
-  else{
-    cout << input << " is not a recognized command.\n";
-  }
-    
+  cout << "Hello, World!" << endl;
   return 0;
 }
